616a60b6e3b785d0dc5d0234116843d883d0f2f1
Terje Nikolaisen <terje.nikolaisen@gmail.com>
2022-12-08 15:13:47 +0100

    gpu

--- apps/test_1.lua
+++ apps/test_1.lua
@@ -1,3 +1,19 @@
+function FindGPU(w, h)
+    gpu = computer.getPCIDevices(findClass("GPU_T1_C"))[1]
+    screen = component.proxy("C1474A8742A4F5D26628068A7821EBB2")
+
+    if not gpu then
+        error("ERROR: GPU is missing in computer.")
+    end
+
+    if not screen then
+        error("ERROR: No large screen connected.")
+    end
+
+    gpu:bindScreen(screen)
+end
+
+
 function ClearScreen(w, h)
     gpu:setBackground(0, 0, 0, 0)
     gpu:setForeground(0, 0, 0, 1)
@@ -12,13 +28,6 @@ function sleep(n)
     while computer.time() - t0 <= n do end
 end
 
-function FindGPU()
-    gpu:setForeground(0, 0, 0, 1)
-    gpu:fill(0, 0, w, h, " ")
-    gpu:flush()
-end
-
-------------------
 
 function sleep(n)
     local t0 = computer.time()
